ca6ab1d3d322223bd03e76d9a109e17e009f5be7
nenadpin <nenadpin@yahoo.com>
2022-11-28 11:32:10 +0100

    react

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,9 +4,10 @@ const Contact = () => {
   return (
     <>
       <iframe
+        style={{ marginLeft: "8px" }}
         title="Mapa Leskovca"
         width="100%"
-        height="95%"
+        height="85%"
         src="https://maps.google.com/maps?q=leskovac%20fina%20apartment&t=&z=13&ie=UTF8&iwloc=&output=embed"
         frameborder="0"
         scrolling="no"
@@ -16,6 +17,14 @@ const Contact = () => {
       <div className="phone">
         <a href="tel:0641227779">0641227779</a>
         <a href="tel:0643459545">0643459545</a>
+      </div>
+      <div className="phone">
+        <a
+          style={{ fontSize: "large" }}
+          href="mailto:nenadpin@yahoo.com?subject=Reservation information"
+        >
+          nenadpin@yahoo.com
+        </a>
       </div>
     </>
   );
